909dc4a6eff0bc9ac098416029dea64226bfcad3
Harold Vinoya <111508176+asahina23@users.noreply.github.com>
2024-08-01 19:50:06 +0800

    Update script.js

--- script.js
+++ script.js
@@ -27,7 +27,7 @@ noButton.addEventListener("click", function () {
 });
 
 function handleYesClick() {
-  titleElement.innerHTML = "Yayyy!! :3";
+  titleElement.innerHTML = "Yayyy!! loveu mae mae ko :3";
   buttonsContainer.classList.add("hidden");
   changeImage("yes");
 }
@@ -44,7 +44,7 @@ function generateMessage(noCount) {
   const messages = [
     "No",
     "Sige na :<",
-    "Lovey please! :<",
+    "Babii please! :<",
     "Don't do this to me :(",
     "You're breaking my heart",
     "I'm gonna cry...",
